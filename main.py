2ef66cbf81f9bb31d0990561685e40a3331d80ab
Kenny Yu <yu.kenny124@gmail.com>
2013-11-14 21:43:03 -0500

    added main driver

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Main driver
+"""
+
+import features
+import learners
+import reduction
+import pandas as pd
+
+if __name__ == "__main__":
+    model = features.BagOfWordsModel(1)
+    reducer = reduction.KernelPCAReduction(2)
+    learner = learners.GaussianNBLearner()
+
+    # TODO: use real data
+    df = pd.DataFrame({'comment' : ['Hop on pop', 'Hop off pop', 'Hop Hop hop'],
+                       'upvotes' : [0, 1, 0]})
+
+    # Make the training set
+    X_train, Y_train = model.make_training_xy(df)
+
+    # Reduce the dimensionality of our training set
+    reducer.fit(X_train)
+    X_train_red = reducer.transform(X_train)
+
+    # Train our learner
+    learner.train(X_train_red, Y_train)
+
+    # Get test data/data from user
+    new_df = pd.DataFrame({'comment' : ['pop off', 'hop hop pop']})
+    X_test = model.data_to_x(new_df)
+    X_test_red = reducer.transform(X_test)
+
+    # Use our learner to predict the new data's label
+    Y_test = learner.predict(X_test_red)
+    new_label = model.y_to_label(df, Y_test)
+    print new_label
