a045bc1d3a9128b6b1527da947b9d99259e96745
Ali Nahm <alisnahm@gmail.com>
2013-12-06 22:29:01 -0500

    adding topscores

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import learners
 import reduction
 import pandas as pd
 
+topscores = {'liberal': 106, 'videos': 10341, 'gentlemanboners': 1619, 'books':
+        4914, 'Music': 7286, 'politics': 15133, 'nba': 4108, 'pokemon': 3270,
+        'funny': 9633, 'technology': 10848, 'conservative': 438, 'food': 3358,
+        'WTF': 11107, 'worldnews': 10559, 'soccer': 2985, 'gaming': 16413,
+        'aww': 7656, 'circlejerk': 3069, 'ladyboners': 1190, 'news': 10995,
+        'television': 9274, 'science': 8965, 'nfl': 5416, 'pics': 19196,
+        'movies': 93504}
+
 if __name__ == "__main__":
     model = features.BagOfWordsModel(1)
     reducer = reduction.KernelPCAReduction(2)
