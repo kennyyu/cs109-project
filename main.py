ca9c3de0abe0afdc6433e72f6a138b2103948295
Joseph Ong <ong.joseph.y@gmail.com>
2013-12-06 19:34:23 -0800

    added cross validation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import features
 import learners
 import reduction
 import pandas as pd
+from sklearn import cross_validation
 
 if __name__ == "__main__":
     model = features.BagOfWordsModel()
@@ -17,10 +18,11 @@ if __name__ == "__main__":
     df = pd.DataFrame({'body' : ['Hop on pop', 'Hop off pop', 'Hop Hop hop'],
                        'ups' : [0, 1, 0],
                        'subreddit' : ['liberal', 'liberal', 'liberal']})
+    
 
     # Make the training set
     X_train, Y_train = model.make_training_xy(df)
-
+    
     # Reduce the dimensionality of our training set
     reducer.fit(X_train)
     X_train_red = reducer.transform(X_train)
@@ -37,4 +39,37 @@ if __name__ == "__main__":
     # Use our learner to predict the new data's label
     Y_test = learner.predict(X_test_red)
     new_label = model.y_to_label(df, Y_test)
-    print new_label
+    print new_label 
+
+    # see how well this model generalizes
+    # test_performance(df)
+   
+def test_performance(df):
+    # transform the input data into feature vectors and labels
+    X, Y = model.make_training_xy(df)
+    kfolds = cross_validation.KFold(len(df.body), n_folds)
+    
+    # for each of the folds, create a training and test set
+    for train_index, test_index in kf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], y[test_index]
+
+        # Reduce the dimensionality of our training set
+        reducer.fit(X_train)
+        X_train_red = reducer.transform(X_train)
+
+        # Train our learner on the reduced features
+        learner.train(X_train_red, Y_train)
+
+        # Apply the same dimensionality reduction to the test set's features
+        # test the performance of the model on the test set
+        X_test_red = reducer.transform(X_test)
+        score = learner.score(X_test_red, y_test)
+
+        print
+        print "--------------"
+        print "TRAINING INDEX:" + str(train_index)
+        print "TEST INDEX:    " + str(test_index)
+        print "SCORE:         " + str(score)
+        print "--------------"
+        print
