baa8e8bd53227fbe1e8f5f9e041c52165e0ea7fc
R.J. Aquino <rjaquino@college.harvard.edu>
2013-12-07 01:36:35 -0500

    Cmdline arguments, CSV output for analytics

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 from main import load_subreddit
+import sys
 
 def comment_num_words(r):
     return len(r['body'].split(' '))
@@ -20,10 +21,22 @@ def capital_letters(r):
     return sum(1 if c.isupper() else 0 for c in r['body'])
 
 if __name__ == "__main__":
+    # functions to add to dataframe
     functions = [comment_num_words, comment_length, avg_word_length, num_letters, num_swear_words]
-    data_file = "data/Liberal.txt"
+
+    # pick datafile
+    if len(sys.argv) < 3:
+        print "Usage: analytics.py datafile outfile"
+        sys.exit()
+
+    data_file = sys.argv[1]
+    outfile = sys.argv[2]
+
     df = load_subreddit(data_file)
-    df = df.head(5)
     for func in functions:
+        print "Applying {}...".format(func.__name__)
         df[func.__name__] = df.apply(lambda x: func(x), axis=1)
-    print df.head()
+
+    # write output
+    df.to_csv(outfile, sep='\t', encoding='utf-8')
+    print "Wrote to {}".format(outfile)
